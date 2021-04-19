0ea58c2b24155f4105c2d5e547e02483319c16bd
MauroLaBarbera <you@example.com>
2021-04-19 18:34:36 +0200

    milestone2

--- data.js/data.js
+++ data.js/data.js
@@ -98,6 +98,16 @@ const icons = [
 ];
 
 
+//CREIAMO UN'ARRAY DI COLORI CHE CI SERVIRANNO POI A COLORARE LE ICONE
+const colori = [
+        '#da0b0b',
+        '#079c1e',
+        '#0e44af',
+];
+
+
+
+
 /**
  * Milestone 1
  * Partendo dalla seguente struttura dati (allegata sotto) , mostriamo in pagina tutte le icone disponibili
@@ -106,13 +116,31 @@ const container = document.querySelector('.icone');
 // console.log(container);
 
 //STAMPA ICONE SU HTML
-stampaIcone(icons,container);
+//stampaIcone(icons,container);
+
+/**
+ * Milestone 2
+ * Coloriamo le icone per tipo1
+ */
+
+//CREIAMO UNA CONST CHE RIPORTERà UNA FUNZIONE CHE ANDRA' A CAMBIARE IL COLORE DELLE ICONE IN BASE AL LORO TIPO ('TYPE')
+const iconeColorate = coloreIcone(icons, colori);
+//STAMPIAMO LE NOSTRE ICONE COLORATE
+stampaIcone(iconeColorate,container);
+
+
+
+
+
+
+
+
 
 
 /*****************************************************************************
                                  FUNZIONI
  ****************************************************************************/
-
+//FUNCTION STAMPA ICONE
 function stampaIcone(icons,container) {
     //CREIAMO UNA VARIABILE 'LET', PERCHE' AL SUO INTERNO AVRA' UN MARKUP VARIABILE CHE ANDREMO AD INSERIRE ALL'INTERNO DEL NOSTRO HTML
     let html = '';
@@ -120,15 +148,62 @@ function stampaIcone(icons,container) {
     //LOOPIAMO SULL'ARRAY DELLE ICONE PER GENERARE IL MARKUP
         icons.forEach( (icona) => {
             //CREIAMO UNA CONST CON ALL'INTERNO GLI OGGETTI CONTENUTI NELL'ARRAY CHE ANDREMO POI AD UTILIZZARE PER RENDERE IL DOCUMENTO PIù FLESSIBILE 
-            const {name, family, prefix} = icona;
+            const {name, family, prefix, color} = icona;
 
             html += 
             `<div class="icona">
-            <i class="${family} ${prefix}${name}
-            style="color: #333"></i>
+            <i class="${family} ${prefix}${name}"
+            style="color: ${color}"></i>
             <div class="nome-icona">${name}</div>
             </div>`
         });
         //AGGIUNGIAMO ICONE AL CONTAINER 'ICONS', STAMPANDOLE POI SUL HTML
         container.innerHTML = html;
+}
+
+
+/**
+ * FUNCTION GENERARE COLORI IN BASE AL TYPE
+ **/
+
+//INSERIAMO UN SECONDO PARAMETRO CHE RAPPRESENTA L'ARRAY DI COLORI CHE ANDRA' APPUNTO A CAMBIARE COLORE ALLE ICONE
+function coloreIcone(icons, colori) {
+    //CREIAMO UNA CONST PER INVOCARE LA FUNZIONE 'genTipo' SULLA QUALE ANDREMO A LOOPARCI I NOSTRI COLORI
+    const tipi = genTipo(icons);
+    console.log(tipi);
+    console.log(colori);
+
+    //ASSEGNAMO IL COLORE ALLE ICONE IN BASE AL TIPO, TRAMITE UN LOOP CON .map
+    const iconeDaColorare = icons.map((icon) =>{
+        //CAPIAMO L'INDICE DI POSIZIONE DELL'ELEMENTO IN UN ARRAY TRAMITE UNA CONST E LA PROPRIETA' '.inderxOf' SULL'ARRAY 'icons'
+        const indiceTipo = tipi.indexOf(icon.type);
+        
+        return {
+            //RITORNIAMO UN 'CLONE' DELL'OGGETTO CONTENUTO IN 'icons' COSì DA NON MANIPOLARE QUELLO ORIGINALE, E LO FACCIAMO TRAMITE UN SPREAD
+            ...icon,
+            //ED AGGIUNGIAMO L'OGGETTO 'color', CHE AVRA' LA PROPRIETA' IN BASE AL TIPO DI ICONA, RICAVANDOLO TRAMITE L'INDICE CREATO PRIMA 'colori[indiceTipo]'
+            color: colori[indiceTipo], 
+        }
+    });
+    console.log(iconeDaColorare);
+    return iconeDaColorare;
+}
+
+
+/**
+ * FUNCTION CHE GENERA UN'ARRAY CON ALL'INTERNO I 'type' CHE UTILIZZEREMO PER INDICARE QUALI ICONE CAMBIARE DI COLORE
+ **/
+
+function genTipo(icons) {
+    //CREIAMO UN NUOVO ARRAY VUOTO CHE POPOLEREMO CON UN LOOP DI TIPO FOREACH
+    const tipi = [];
+    //LOOP SULL'ARRAY DI 'ICONS'
+    icons.forEach( (icon) => {
+        //SE GLI OGGETTI CONTENUTI ALL'INTERNO DELL'ARRAY 'tipi'' NON HANNO L'ATTRIBUTO 'type', PUSHIAMOLO DENTRO TRAMITE UN IF
+        if (! tipi.includes(icon.type)) {
+            tipi.push(icon.type);
+        } 
+    });
+    //RITORNIAMO IL RISULTATO DELLA NOSTRA FUNZIONE
+    return tipi;
 }
