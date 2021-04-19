481c50d5133c6a18093f16d1a51d87397575a678
MauroLaBarbera <you@example.com>
2021-04-19 20:07:29 +0200

    finito

--- data.js/data.js
+++ data.js/data.js
@@ -129,6 +129,39 @@ const iconeColorate = coloreIcone(icons, colori);
 stampaIcone(iconeColorate,container);
 
 
+/**
+ * Milestone 3
+ * Creiamo una select con i tipi di icone e usiamola per filtrare le icone
+ */
+
+/**
+ * MODIFICA 'PLACEHORDER' IN SELECT
+ */
+//RICHIAMANDO L'ID DEL SELECT SU HTML
+const selezione = document.querySelector('#type');
+//CREIAMO UNA CONST CHE ANDRA' AD INSERIRE LE OPZIONI IN BASE AL type RICHIAMANDO LA FUNZIONE 'genTipo'  
+const tipi = genTipo(iconeColorate);
+//GENERIAMO LE OPZIONI DI SELEZIONE
+genOpzione(tipi, selezione)
+
+
+/**
+ * FILTRO ICONE IN BASE AL TIPO
+ */
+//CREIAMO UN EVENTO IN BASE ALLA SELEZIONE DELL'UTENTE
+selezione.addEventListener('change', () => { //SI USA CHANGE PERCHE' L'EVENTO DEVE AVVENIRE AL CAMBIAMENTO DELLA PROPRIETA' E NON AL SUO CLICK
+    //DOBBIAMO PRELEVARE IL VALORE PER CAPIRE COSA IN COSA EFFETTIVAMENTE SIA CAMBIATA LA SELEZIONE
+    // console.log(selezione.value);
+    const selezionato = selezione.value;
+    //FUNZIONE CHE MI RIPORTA UN ARRAY 'FILTRATO' IN BASE ALLA SELEZIONE FATTA
+    const iconeFiltrate = filtroIcone(iconeColorate, selezionato);
+    stampaIcone(iconeFiltrate,container);
+
+}); 
+
+
+
+
 
 
 
@@ -170,8 +203,8 @@ function stampaIcone(icons,container) {
 function coloreIcone(icons, colori) {
     //CREIAMO UNA CONST PER INVOCARE LA FUNZIONE 'genTipo' SULLA QUALE ANDREMO A LOOPARCI I NOSTRI COLORI
     const tipi = genTipo(icons);
-    console.log(tipi);
-    console.log(colori);
+    // console.log(tipi);
+    // console.log(colori);
 
     //ASSEGNAMO IL COLORE ALLE ICONE IN BASE AL TIPO, TRAMITE UN LOOP CON .map
     const iconeDaColorare = icons.map((icon) =>{
@@ -191,7 +224,7 @@ function coloreIcone(icons, colori) {
 
 
 /**
- * FUNCTION CHE GENERA UN'ARRAY CON ALL'INTERNO I 'type' CHE UTILIZZEREMO PER INDICARE QUALI ICONE CAMBIARE DI COLORE
+ * FUNCTION CHE GENERA UN'ARRAY CON ALL'INTERNO I 'type' CHE UTILIZZEREMO PER INDICARE QUALI ICONE DEVONO CAMBIARE COLORE
  **/
 
 function genTipo(icons) {
@@ -206,4 +239,43 @@ function genTipo(icons) {
     });
     //RITORNIAMO IL RISULTATO DELLA NOSTRA FUNZIONE
     return tipi;
+}
+
+
+/**
+ * FUNCTION CHE GENERA OPZIONE IN BASE AL FILTRO
+ */
+function genOpzione(tipi, selezione) {
+    //GEN OPZIONE
+    let opzione = '';
+    tipi.forEach((tipo) => {
+        //AGGIUNGIAMO UN PO' DI HTML IN MODO DA MODIFICARE LE OPZIONI IN BASE AL type
+        opzione += `<option value="${tipo}">${tipo}</option>`; 
+    });
+    //UTILIZZIAMO IL '+=' IN MODO TALE DA NON SOVRASCRIVERE "TUTTO" PRESENTE DI DEFAULT DENTRO L'HTML
+    selezione.innerHTML += opzione;
+}
+console.log(selezione);
+
+/**
+ * FUNCTION ICONE FILTRATE
+ */
+
+function filtroIcone(icons, selezionato) { 
+
+    const filtraggio = icons.filter((icona) => { //RITORNA NUOVO ARRAY SENZA TOCCARE QUELLO ORIGINALE
+        //RITORNIAMO L'ARRAY CHE CONTERRA' LE ICONE DEL TIPO CHE ABBIAMO SELEZIONATO
+
+        //VISTO CHE NON ABBIAMO UN type  'tutto', NEL MOMENTO IN CUI RICLICCHEREMO SU DI ESSO, NON CI MOSTRERA' PIù NULLA. 
+        //PER OVVIARE A QUESTO PROBLEMA DOBBIAMO CREARE UNA CONDIZIONE CHE NEL CASO NOI CLICCASSIMO DI NUOVO 'tutto', RICOMPARIRANNO DI NUOVO TUTTE LE ICONE
+        if(selezionato === 'tutto') {
+            return icons
+        }
+
+        return icona.type === selezionato;
+    }); 
+
+    //RITORNIAMO SULL'HTML IL RISULTATO DELLE ICONE SELEZIOANTE, INDICANDO LA CONST CREATA ALL'INIZIO DELLA FUNZIONE
+    return filtraggio;
+
 }
